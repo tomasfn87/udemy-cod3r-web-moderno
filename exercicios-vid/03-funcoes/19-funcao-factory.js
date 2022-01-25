d6d0942d48c5bc6259e5b9115032c1cee167dd92
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-01-24 22:21:43 -0300

    Atualizando ".gitignore".

--- exercicios-vid/03-funcoes/19-funcao-factory.js
+++ exercicios-vid/03-funcoes/19-funcao-factory.js
@@ -1,8 +1,8 @@
 // Factory simples
-function createPerson() {
+function createPerson(nome, sobrenome) {
     return {
-        nome: "Ana Clara",
-        sobrenome: "Caviar"
+        nome: nome,
+        sobrenome: sobrenome
     }
 }
 
@@ -10,12 +10,12 @@ function createProduct(nome, tipo, preco, desconto=0.1) {
     return {
         nome,
         tipo,
-        preco: "R$" + preco.toPrecision(3),
-        preco_final: "R$" + (preco - (preco * desconto)).toPrecision(3)
+        preco: `R$${preco.toPrecision(3)}`,
+        preco_final: `R$${(preco - (preco * desconto)).toPrecision(3)}`
     }
 }
 
-console.log(createPerson())
+console.log(createPerson("Ana Clara", "Caviar"))
 console.log(createProduct("Água mineral", "Bebida", 2.5))
 console.log(createProduct("Coca Cola", "Bebida", 4.495))
 console.log(createProduct("Tubaína", "Bebida", 4.995, 0.2))
--- exercicios-vid/03-funcoes/21-desafio-func-construtora.js
+++ exercicios-vid/03-funcoes/21-desafio-func-construtora.js
@@ -1,8 +1,8 @@
-function criarPessoa(nome) {
+function Pessoa(nome) {
     this.falar = () => {
-        console.log(`Meu nome é ${nome}.`)
+        console.log(`Me chamo ${nome}. Prazer em conhecê-los.`)
     }
 }
 
-const p2 = new criarPessoa("Arquimedes")
+const p2 = new Pessoa("Arquimedes")
 p2.falar()

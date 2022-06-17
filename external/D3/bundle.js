5bca85cca74c58e3ce6c7d16dd2e29a25d3f56cb
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-06-17 13:43:47 -0300

    amend

---
+++ external/D3/bundle.js
@@ -0,0 +1,6 @@
+(function () {
+    'use strict';
+
+    const svg = d3.select('svg');
+    svg.style('background-color', '#202020');
+}(d3));
---
+++ external/D3/index.js
@@ -0,0 +1,4 @@
+import { select } from 'd3';
+
+const svg = select('svg');
+svg.style('background-color', 'red');

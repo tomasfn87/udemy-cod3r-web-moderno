b1b34f433c781c2a4654e7b978d07f0dbf884348
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-06-19 21:43:03 -0300

    Alterados arquivos da pasta '/univesp/Fundamentos de Internet e Web/sem6': 1): 'counter_increment.css': pequenos ajustes em '.post:after' e '.onclick-content'. 2): 'counter_increment.html': removida a chamada da função 'toggleOnclickContent' em <body>; adicionadas duas chamadas da função 'toggleOnclickContent' a cada <section>: 'onclick="toggleOnclickContent(id)"' e 'onmouseup="toggleOnclickContent(id)"'. 3): 'getTitle.js': alterada a função 'toggleOnclickContent': agora a função recebe o id do elemento a ser capturado. Alterado o nome das funções: 'getChildNodeByTagName' -> 'getChildNodeIndexByTagName'; 'getChildNodeByClassName' -> 'getChildNodeIndexByClassName'.

--- univesp/Fundamentos de Internet e Web/sem6/getTitle.js
+++ univesp/Fundamentos de Internet e Web/sem6/getTitle.js
@@ -2,12 +2,11 @@ const getTitle = (element_class, tag) => {
   titulos = document.getElementsByClassName(element_class)
   for (t of titulos) {
     child_nodes = t.parentNode.parentNode.childNodes // <-- ajustar o nível de profundidade do elemento (remover ou acrescentar .parentNode)
-    i = getChildNodeByTagName(child_nodes, tag)
-    t.innerText = child_nodes[i].innerText.toLowerCase()
+    t.innerText = child_nodes[getChildNodeIndexByTagName(child_nodes, tag)].innerText.toLowerCase()
   }
 }
 
-const getChildNodeByTagName = (child_nodes, element_tag) => {
+const getChildNodeIndexByTagName = (child_nodes, element_tag) => {
   for (i in child_nodes) {
     if (child_nodes[i].localName == element_tag) {
       return i
@@ -15,20 +14,21 @@ const getChildNodeByTagName = (child_nodes, element_tag) => {
   }
 }
 
-(function toggleOnclickContent() {
-    document.addEventListener('click', ((e) => {
-      element_list = e.path
-      child_nodes = element_list[getChildNodeByTagName(element_list, 'section')].childNodes
-      onclick_content = child_nodes[getChildNodeByClassName(child_nodes, 'onclick-content')]
-      if (onclick_content.style.display == '' || onclick_content.style.display == 'none') {
-        onclick_content.style.display = 'block'
-      } else {
-        onclick_content.style.display = 'none'
-      }
-    }))
-})()
+const toggleOnclickContent = (id) => {
+  section = document.getElementById(id)
+  section.addEventListener('click', () => {
+    child_nodes = section.childNodes
+    onclick_content_index = getChildNodeIndexByClassName(child_nodes, 'onclick-content')
+    onclick_content = child_nodes[onclick_content_index]
+    if (!onclick_content.style.display || onclick_content.style.display == 'none') {
+      onclick_content.style.display = 'block'
+    } else {
+      onclick_content.style.display = 'none'
+    }
+  })
+}
 
-const getChildNodeByClassName = (child_nodes, element_class) => {
+const getChildNodeIndexByClassName = (child_nodes, element_class) => {
   for (i in child_nodes) {
     if (child_nodes[i].className == element_class) {
       return i

9c1f45805c556daf87032090aac20b5b1c4a7986
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-06-23 20:47:24 -0300

    Alterado arquivo '/WA/remove_quote_near_double_curly_brackets.js': 1): agora o arquivo contém apenas as funções - a interface CLI foi movida para outro arquivo: 'CLI_remove_quote_near_double_curly_brackets.js'. 2): as funções do arquivo foram exportadas.

--- WA/remove_quote_near_double_curly_brackets.js
+++ WA/remove_quote_near_double_curly_brackets.js
@@ -22,7 +22,7 @@ const remove_quote_near_double_curly_brackets = (input_file, output_file) => {
   })
 }
 
-const check_file_extension = (file, ext) => {
+export const check_file_extension = (file, ext) => {
   ext = ext.replace(/\./, '')
   const fSplit = file.split('.')
   const fileExt = fSplit[fSplit.length-1]
@@ -32,20 +32,4 @@ const check_file_extension = (file, ext) => {
   return false
 }
 
-let inputs = process.argv
-if (inputs.length < 4) {
-  console.error('ERRO: são necessários 2 parâmetros:\n   1. Caminho do arquivo de origem           \(a ser limpo\)\n   2. Caminho para salvar o arquivo limpo    \(o resultado\) \n\nSerá checado se o arquivo de destino é diferente do arquivo de origem:\n   - Se for salvar na mesma pasta, deverá usar um nome diferente')
-} else {
-  const in_file = inputs[2]
-  const out_file = inputs[3]
-
-  if (in_file == out_file) {
-    console.error('ERRO: o arquivo de saída tem que ser diferente do arquivo de entrada, ou ser salvo em outra pasta.')
-  } else if (!check_file_extension(in_file, 'js')) {
-    console.error('ERRO: o arquivo de entrada deve ter extensão .js.')
-  } else if (!check_file_extension(out_file, 'js')) {
-    console.error('ERRO: o arquivo de saída deve ter extensão .js.')
-  } else {
-    remove_quote_near_double_curly_brackets(in_file, out_file)
-  }
-}
+export default remove_quote_near_double_curly_brackets

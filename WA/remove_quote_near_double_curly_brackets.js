ad34843bc3b0d8ef3cb9c4d23f4bda7086853a11
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-06-23 21:19:15 -0300

    Alterado o arquivo '/WA/remove_quote_near_double_curly_brackets.js': o arquivo contém apenas a função que limpa a string, removendo aspas simples ou duplas perto de colchetes duplos, seguindo o padrão: '{{, "{{, }}' ou }}". A função 'check_file_extension' foi movida para o arquivo 'check_file_extension.js'.

--- WA/remove_quote_near_double_curly_brackets.js
+++ WA/remove_quote_near_double_curly_brackets.js
@@ -1,35 +1,8 @@
-import fs from 'fs'
-
-const remove_quote_near_double_curly_brackets = (input_file, output_file) => {
-  fs.readFile(input_file, 'utf-8', (err, data) => {
-    err && console.error(err)
-    if (!data) {
-      return
-    }
-    console.log(' - Antes da limpeza:\n' + data)
-
-    const regex = /('|")(\{\{)|(\}\})('|")/g
-    const matches = data.match(regex)
-    console.log('\n - Matchs \(' + matches.length + '\):\n' + matches)
-
-    const filtered_data = data.replaceAll(regex, "$2$3")
-    console.log('\n - Depois da limpeza:\n' + filtered_data)
-
-    fs.writeFile(output_file, filtered_data, (err) => {
-      err && console.err(err)
-      return
-    })
-  })
-}
-
-export const check_file_extension = (file, ext) => {
-  ext = ext.replace(/\./, '')
-  const fSplit = file.split('.')
-  const fileExt = fSplit[fSplit.length-1]
-  if (fileExt == ext) {
-    return true
-  }
-  return false
+const remove_quote_near_double_curly_brackets = (string, loud=false) => {
+  const regex = /('|")(\{\{)|(\}\})('|")/g
+  const matches = string.match(regex)
+  loud && console.log('\n - Matchs \(' + matches.length + '\):\n' + matches) 
+  return string.replaceAll(regex, "$2$3")
 }
 
 export default remove_quote_near_double_curly_brackets

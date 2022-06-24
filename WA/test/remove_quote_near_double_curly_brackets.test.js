1f3554b691c747bfcc2a350f0f4e493deea8c004
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-06-23 21:26:36 -0300

    Adicionado o arquivo '/WA/test/remove_quote_near_double_curly_brackets.test.js' o arquivo contém testes para a função 'remove_quote_near_double_curly_brackets'.

--- WA/test/remove_quote_near_double_curly_brackets.test.js
+++ WA/test/remove_quote_near_double_curly_brackets.test.js
@@ -1,15 +1,15 @@
-import { check_file_extension } from '../remove_quote_near_double_curly_brackets.js'
+import remove_quote_near_double_curly_brackets from '../remove_quote_near_double_curly_brackets.js'
 import { expect } from "chai"
 
 
-describe('Chech file extension', () => {
-  it("check_file_extension('test.js',  'js'   --> true)", () => {
-    let result = check_file_extension('test.js', 'js')
-    expect(result).to.equal(true)
+describe('Remove Quote Near Double Curly Brackets', () => {
+  it("remove_quote_near_double_curly_brackets('\"{{Test}}\"') --> '{{Test}}'", () => {
+    let result = remove_quote_near_double_curly_brackets('"{{Test}}"')
+    expect(result).to.equal('{{Test}}')
   })
 
-  it("check_file_extension('test.jpg', 'png'  --> false)", () => {
-    let result = check_file_extension('test.jpg', 'png')
-    expect(result).to.equal(false)
+  it("remove_quote_near_double_curly_brackets(\"'{{Test}}'\") --> \"{{Test}}\"", () => {
+    let result = remove_quote_near_double_curly_brackets("'{{Test}}'")
+    expect(result).to.equal("{{Test}}")
   })
 })

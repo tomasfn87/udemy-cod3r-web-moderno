87f1f8f29d779a4ce7b32da3c2b673e368b0dd09
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-06-23 22:33:23 -0300

    Alterado arquivo 'test/check_file_extension.test.js': adicionados dois testes, para os casos onde a extensão é fornecida com '.', como por exemplo: '.txt', que produz o mesmo resultado que simplesmente 'txt'.

--- WA/test/check_file_extension.test.js
+++ WA/test/check_file_extension.test.js
@@ -2,14 +2,24 @@ import check_file_extension from '../check_file_extension.js'
 import { expect } from "chai"
 
 
-describe('Chech file extension', () => {
-  it("check_file_extension('test.js',  'js')   --> true)", () => {
+describe('Check File Extension', () => {
+  it("check_file_extension('test.js',  'js')    --> true)", () => {
     let result = check_file_extension('test.js', 'js')
     expect(result).to.equal(true)
   })
 
-  it("check_file_extension('test.jpg', 'png')  --> false)", () => {
+  it("check_file_extension('test.jpg', 'png')   --> false)", () => {
     let result = check_file_extension('test.jpg', 'png')
+    expect(result).to.equal(false)
+  })
+
+  it("check_file_extension('test.js',  '.js')   --> true)", () => {
+    let result = check_file_extension('test.js', '.js')
+    expect(result).to.equal(true)
+  })
+
+  it("check_file_extension('test.jpg', '.png')  --> false)", () => {
+    let result = check_file_extension('test.jpg', '.png')
     expect(result).to.equal(false)
   })
 })

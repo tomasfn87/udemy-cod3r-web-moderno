6aa5e42ef0053789e206b5a99859665139707831
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-06-23 21:13:43 -0300

    Renomeado arquivo '/WA/CLI_CLI_remove_quote_near_double_curly_brackets.js' --> 'get_file_without_quote_near_double_curly_brackets.js'.

--- WA/get_file_without_quote_near_double_curly_brackets.js
+++ WA/get_file_without_quote_near_double_curly_brackets.js
@@ -1,7 +1,27 @@
-import remove_quote_near_double_curly_brackets, { check_file_extension } from './remove_quote_near_double_curly_brackets.js'
+import fs from 'fs'
+
+import remove_quote_near_double_curly_brackets from './remove_quote_near_double_curly_brackets.js'
+import check_file_extension from './check_file_extension.js'
+
+
+const get_file_without_quote_near_double_curly_brackets = (input_file, output_file) => {
+  fs.readFile(input_file, 'utf-8', (err, data) => {
+    err && console.error(err)
+    if (!data) {
+      return
+    }
+    console.log(' - Antes da limpeza:\n' + data)
+    const filtered_data = remove_quote_near_double_curly_brackets(data, true)
+    console.log('\n - Depois da limpeza:\n' + filtered_data)
+    fs.writeFile(output_file, filtered_data, (err) => {
+      err && console.err(err)
+      return
+    })
+  })
+}
 
 let inputs = process.argv
-if (inputs.length > 2 && inputs.length < 4) {
+if (inputs.length < 4) {
   console.error('ERRO: são necessários 2 parâmetros:\n   1. Caminho do arquivo de origem           \(a ser limpo\)\n   2. Caminho para salvar o arquivo limpo    \(o resultado\) \n\nSerá checado se o arquivo de destino é diferente do arquivo de origem:\n   - Se for salvar na mesma pasta, deverá usar um nome diferente')
 } else {
   const in_file = inputs[2]
@@ -14,6 +34,6 @@ if (inputs.length > 2 && inputs.length < 4) {
   } else if (!check_file_extension(out_file, 'js')) {
     console.error('ERRO: o arquivo de saída deve ter extensão .js.')
   } else {
-    remove_quote_near_double_curly_brackets(in_file, out_file)
+    get_file_without_quote_near_double_curly_brackets(in_file, out_file)
   }
 }

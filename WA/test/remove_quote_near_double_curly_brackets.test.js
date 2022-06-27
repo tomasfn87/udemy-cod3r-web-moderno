c7b3d3836fd6ac174b37ef854d3a1e8fab38be95
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-06-26 21:27:38 -0300

    Alterado arquivo '/WA/test/remove_quote_near_double_curly_brackets.test.js': adicionados dois testes unitários para os casos onde aspas simples ou duplas estão perto de '{{' ou '}}', porém do lado incorreto, e portanto, não devem ser retiradas.

--- WA/test/remove_quote_near_double_curly_brackets.test.js
+++ WA/test/remove_quote_near_double_curly_brackets.test.js
@@ -3,13 +3,23 @@ import { expect } from "chai"
 
 
 describe('Remove Quote Near Double Curly Brackets', () => {
-  it("remove_quote_near_double_curly_brackets('\"{{Test}}\"') --> '{{Test}}'", () => {
+  it("remove_quote_near_double_curly_brackets('\"{{Test}}\"')   --> '{{Test}}'", () => {
     let result = remove_quote_near_double_curly_brackets('"{{Test}}"')
     expect(result).to.equal('{{Test}}')
   })
 
-  it("remove_quote_near_double_curly_brackets(\"'{{Test}}'\") --> \"{{Test}}\"", () => {
+  it("remove_quote_near_double_curly_brackets(\"'{{Test}}'\")   --> \"{{Test}}\"", () => {
     let result = remove_quote_near_double_curly_brackets("'{{Test}}'")
     expect(result).to.equal("{{Test}}")
+  })
+
+  it("remove_quote_near_double_curly_brackets(\"{{'Test'}}'\")  --> \"{{'Test'}}\"", () => {
+    let result = remove_quote_near_double_curly_brackets("{{'Test'}}")
+    expect(result).to.equal("{{'Test'}}")
+  })
+
+  it("remove_quote_near_double_curly_brackets('{{\"Test\"}}')   --> '{{\"Test\"}}'", () => {
+    let result = remove_quote_near_double_curly_brackets('{{"Test"}}')
+    expect(result).to.equal('{{"Test"}}')
   })
 })

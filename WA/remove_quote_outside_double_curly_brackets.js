b12c87591007cf863d212a863b09f217810fd2ab
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-06-30 10:49:10 -0300

    Alterado o arquivo '/WA/remove_quote_outside_double_curly_brackets.js': adicionado teste que veficar se existe alguma substituição a ser feita antes de fazê-la, o mesmo valendo para a mensagem que mostra os termos correspondente encontrados .

--- WA/remove_quote_outside_double_curly_brackets.js
+++ WA/remove_quote_outside_double_curly_brackets.js
@@ -1,8 +1,11 @@
 const remove_quote_outside_double_curly_brackets = (string, loud=false) => {
   const regex = /('|")(\{{2})|(\}{2})('|")/g
   const matches = string.match(regex)
-  loud && console.log('\n - Removendo aspas simples ou duplas fora de {{ ou }} \(' + matches.length + ' ocorrências\):\n' + matches)
-  return string.replaceAll(regex, "$2$3")
+  if (regex.test(string)) {
+    loud && console.log('\n - Removendo aspas simples ou duplas fora de {{ ou }} \(' + matches.length + ' ocorrências\):\n' + matches)
+    return string.replaceAll(regex, "$2$3")
+  }
+  return string
 }
 
 export default remove_quote_outside_double_curly_brackets
